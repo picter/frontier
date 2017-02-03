52a4d1039c2017f1993674fab4a6ef943babb922
Richard <hello@noxan.net>
2017-02-03 18:39:13 +0100

    check for index config file in folder

--- src/index.js
+++ src/index.js
@@ -14,8 +14,10 @@ const baseDirectory = path.join(cwd, directoryParam)
 const files = fs.readdirSync(baseDirectory)
 
 const ignoreFiles = ['assets']
+const indexFile = path.join(baseDirectory, 'index.ini')
 
 const filteredFiles = files.filter(
   file => !ignoreFiles.includes(file)
 )
 console.log(filteredFiles)
+console.log('Has index file:', fs.existsSync(indexFile))
