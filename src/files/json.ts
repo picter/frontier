2ae76b3ef301d54e9d4f32a5358b24effa1eb050
Richard <hello@noxan.net>
2017-04-22 23:39:01 +0200

    fix all linting warnings

--- src/files/json.ts
+++ src/files/json.ts
@@ -1,17 +1,17 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import * as handlebars from 'handlebars'
+import * as fs from 'fs';
+import * as path from 'path';
+import * as handlebars from 'handlebars';
 
-const cwd = process.cwd()
+const cwd = process.cwd();
 
 export const renderJsonFile = (template, content) => {
-  const templatePath = path.join(cwd, 'templates', template + '.hbs')
-  const templateContent = fs.readFileSync(templatePath, 'utf-8')
-  const templateHbs = handlebars.compile(templateContent)
-  return templateHbs(content)
-}
+  const templatePath = path.join(cwd, 'templates', template + '.hbs');
+  const templateContent = fs.readFileSync(templatePath, 'utf-8');
+  const templateHbs = handlebars.compile(templateContent);
+  return templateHbs(content);
+};
 
 export default fileContent => {
-  const { template, content } = JSON.parse(fileContent)
-  return renderJsonFile(template, content)
-}
+  const { template, content } = JSON.parse(fileContent);
+  return renderJsonFile(template, content);
+};
