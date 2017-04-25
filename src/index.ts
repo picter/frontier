1501f8ca9301c82525ae6387456c126e7b4b6ef0
Richard <hello@noxan.net>
2017-04-25 15:30:20 +0200

    load data from mock file and replace based on mapping object

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,19 @@ const indexContent = Object.assign({}, {
   content: new handlebars.SafeString(content),
 }, indexFileContent);
 
-const result = renderJsonFile('index', indexContent);
+let result = renderJsonFile('index', indexContent);
+
+const callFieldMapping = {
+  CALL_OPEN_DATE: 'submissionStartDate',
+  CALL_CLOSE_DATE: 'submissionEndDate',
+};
+
+const apertureCallData = require('../mocks/request-aperture.json');
+
+Object.keys(callFieldMapping).forEach(key => {
+  const callField = callFieldMapping[key];
+  const value = apertureCallData[callField];
+  result = result.replace(`%${key}%`, value);
+});
 
 console.log(result);
