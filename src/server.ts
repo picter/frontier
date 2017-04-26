4a7d5e32f54ca79807abb7b14fea3da5d20235db
Richard <hello@noxan.net>
2017-04-26 13:25:26 +0200

    detect requests to stylesheet and to asset files

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,13 @@ app.use(ctx => {
   if (url.charAt(0) === '/') {
     url = url.substring(1);
   }
-  ctx.body = renderPage(url);
+  if (url.endsWith('styles.css')) {
+    ctx.status = 404;
+  } else if (url.includes('assets')) {
+    ctx.status = 404;
+  } else {
+    ctx.body = renderPage(url);
+  }
 });
 
 app.listen(3141);
