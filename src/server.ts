4fffedcf6f39474e69303ac59f10e560bcc80ce4
Richard <hello@noxan.net>
2017-04-26 13:15:54 +0200

    render page matching path (might be dangerous, use ONLY IN DEVELOPMENT)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import * as Application from 'koa';
 
+import { renderPage } from './renderer';
+
 const app = new Application();
 
-app.use(ctx => ctx.body = 'Hello Picter!');
+app.use(ctx => {
+  let url = ctx.url;
+  if (url.charAt(0) === '/') {
+    url = url.substring(1);
+  }
+  ctx.body = renderPage(url);
+});
 
 app.listen(3141);
