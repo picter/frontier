51c8f579faf506da30796d12e3c00ec07bbb1153
Richard <hello@noxan.net>
2017-04-26 13:07:51 +0200

    extract page rendering to dedicated function and file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import * as process from 'process';
-import * as handlebars from 'handlebars';
 import * as winston from 'winston';
-import * as ini from 'ini';
 
-import { renderJsonFile } from './files/json';
-import templates from './templates';
-import { renderFile } from './renderer';
-import postprocess from './postprocess';
+import { renderPage } from './renderer';
 
 winston.level = 'debug';
 
@@ -21,36 +15,4 @@ if (process.argv.length < 3) {
 const directoryParam = process.argv[2];
 const baseDirectory = path.join(cwd, directoryParam);
 
-const allFiles = fs.readdirSync(baseDirectory);
-
-const ignoreFiles = ['assets', 'index.ini', 'theme.sass'];
-const indexFile = path.join(baseDirectory, 'index.ini');
-const themeFile = path.join(baseDirectory, 'theme.sass');
-
-const files = allFiles.filter(
-  file => !ignoreFiles.includes(file),
-);
-winston.debug('Files:', files);
-winston.debug('Has index file:', fs.existsSync(indexFile));
-winston.debug('Has theme file:', fs.existsSync(themeFile));
-
-winston.debug('Templates:', templates);
-
-// Render
-const indexOfFile = filename => parseInt(filename.split('-')[0], 10);
-
-const content = files.sort((first, second) =>
-  indexOfFile(first) - indexOfFile(second),
-).map(
-  file => renderFile(baseDirectory, file),
-).join('\n');
-
-const indexFileContent = ini.parse(fs.readFileSync(indexFile, 'utf-8'));
-
-const indexContent = Object.assign({}, {
-  content: new handlebars.SafeString(content),
-}, indexFileContent);
-
-const result = postprocess(renderJsonFile('index', indexContent));
-
-console.log(result);
+console.log(renderPage(baseDirectory));
