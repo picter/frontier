4f7817d3d574a670bf20181b4660c934e9e10083
Richard <hello@noxan.net>
2017-05-14 22:44:43 +0200

    setup commander for build sub command

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,1 +1,8 @@
-console.log('build');
+import * as process from 'process';
+import * as program from 'commander';
+
+program
+  .usage('[options] <path>')
+  .parse(process.argv);
+
+console.log(program.args);
