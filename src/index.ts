2abb89034d2ff5ae9ed6994fba76136291245550
Richard <hello@noxan.net>
2017-05-14 22:11:31 +0200

    split old building logic into dedicated file

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,6 @@
-import * as path from 'path';
-import * as process from 'process';
 import * as winston from 'winston';
-
-import { renderPage } from './renderer';
+import * as process from 'process';
 
 winston.level = 'debug';
 
-const cwd = process.cwd();
-
-if (process.argv.length < 3) {
-  throw new Error('Please provide an base directory as parameter.');
-}
-
-const directoryParam = process.argv[2];
-const baseDirectory = path.join(cwd, directoryParam);
-
-console.log(renderPage(baseDirectory));
+winston.debug(process.argv);
